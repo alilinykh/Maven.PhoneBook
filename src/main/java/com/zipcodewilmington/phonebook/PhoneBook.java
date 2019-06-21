7e14eabcac62373b5f068664a88e30cc9a251a0c
alilinykh <aililnykh@dtcc.edu>
2019-06-20 20:50:22 -0400

    done up to reverse lookup

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,5 +1,6 @@
 package com.zipcodewilmington.phonebook;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -7,32 +8,53 @@ import java.util.Map;
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
+    Map<String,List<String>> map;
+    List<String> list = new ArrayList<>();
+
     public PhoneBook(Map<String, List<String>> map) {
+        this.map = map;
     }
 
     public PhoneBook() {
+
         this(null);
     }
 
     public void add(String name, String phoneNumber) {
+        list.add(phoneNumber);
+        map.put(name,list);
     }
 
     public void addAll(String name, String... phoneNumbers) {
+        for (String s: phoneNumbers
+             ) {
+            list.add(s);
+        }
+        map.put(name,list);
     }
 
     public void remove(String name) {
+        map.remove(name);
     }
 
     public Boolean hasEntry(String name) {
-        return null;
+        return map.containsKey(name);
     }
 
     public List<String> lookup(String name) {
-        return null;
+        return map.get(name);
     }
 
     public String reverseLookup(String phoneNumber)  {
-        return null;
+        for (Map.Entry <String,List<String>> entry: map.entrySet()
+             ) {
+            for (String number: entry
+                 ) {
+
+            }
+        }
+
+        return ;
     }
 
     public List<String> getAllContactNames() {
